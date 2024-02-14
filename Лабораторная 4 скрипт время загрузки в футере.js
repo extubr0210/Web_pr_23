cfa9b74701f4c30911229c3f5ba3ce3b2a8ce21f
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2024-02-14 13:09:36 +0300

    Delete Лабораторная 4 скрипт время загрузки в футере.js

--- Лабораторная 4 скрипт время загрузки в футере.js
+++
@@ -1,6 +0,0 @@
-(function(){
-var loadtime = Date.now() -
- window.performance.timing.navigationStart;
-document.getElementById("ftd1").innerHTML = "Время загрузки страницы = "+loadtime+" миллисекунд";
-
-})();
