f81fd47e8839a1b1e716403d8fc185d4d41489e5
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2024-02-14 19:59:48 +0300

    Update Лабораторная 6 скрипты.js

--- Скрипты/Лабораторная 6 скрипты.js
+++ Скрипты/Лабораторная 6 скрипты.js
@@ -3,10 +3,9 @@ let rdm1=Math.ceil(rndm * 200 + 2);
 alert("wrtwr "+rdm1);
 const Jsp1=document.getElementById("jsnplhld1");
 
-
 fetch("https://jsonplaceholder.typicode.com/todos/"+rdm1)
 .then((response) => response.json())
 .then((json) => {
-console.log(json);
-Jsp1.innerHTML="<p>"+JSON.stringify(json)+"</p>";
+    console.log(json);
+    Jsp1.innerHTML="<p>"+JSON.stringify(json)+"</p>";
 });
