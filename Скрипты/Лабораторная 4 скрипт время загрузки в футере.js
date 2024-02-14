3fc19849288327908613cdd3af01a25f73f46191
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2024-02-14 19:57:33 +0300

    Update Лабораторная 4 скрипт время загрузки в футере.js

--- Скрипты/Лабораторная 4 скрипт время загрузки в футере.js
+++ Скрипты/Лабораторная 4 скрипт время загрузки в футере.js
@@ -1,6 +1,6 @@
 (function(){
-var loadtime = Date.now() -
- window.performance.timing.navigationStart;
-document.getElementById("ftd1").innerHTML = "Время загрузки страницы = "+loadtime+" миллисекунд";
+    var loadtime = Date.now() -
+    window.performance.timing.navigationStart;
+    document.getElementById("ftd1").innerHTML = "Время загрузки страницы = "+loadtime+" миллисекунд";
 
 })();
