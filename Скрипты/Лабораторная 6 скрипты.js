fe430fed8eaba63b2cb8a237d1bc4e0f41e3da62
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2024-02-14 13:11:12 +0300

    Add files via upload

---
+++ Скрипты/Лабораторная 6 скрипты.js
@@ -0,0 +1,12 @@
+const rndm=Math.random();
+let rdm1=Math.ceil(rndm * 200 + 2);
+alert("wrtwr "+rdm1);
+const Jsp1=document.getElementById("jsnplhld1");
+
+
+fetch("https://jsonplaceholder.typicode.com/todos/"+rdm1)
+.then((response) => response.json())
+.then((json) => {
+console.log(json);
+Jsp1.innerHTML="<p>"+JSON.stringify(json)+"</p>";
+});
