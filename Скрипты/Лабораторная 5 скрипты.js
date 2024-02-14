3a9986eb55045b30547c7e3cb6e6618b140b8d5f
extubr0210 <62099167+extubr0210@users.noreply.github.com>
2024-02-14 19:58:34 +0300

    Update Лабораторная 5 скрипты.js

--- Скрипты/Лабораторная 5 скрипты.js
+++ Скрипты/Лабораторная 5 скрипты.js
@@ -4,12 +4,11 @@ const Tbl=document.getElementById("tabl1");
 Frm.style.color="red";
 
 function getFrmVale(event){
-event.preventDefault();
-const nofrows = Frm.querySelector('[name="number_frm_form"]');
-
-const aedtxt = Frm.querySelector('[name="added_text"]');
-alert(nofrows.value+" "+aedtxt.value);
-Tbl.innerHTML=Tbl.innerHTML+" <p>"+aedtxt.value+"</p>";
+    event.preventDefault();
+    const nofrows = Frm.querySelector('[name="number_frm_form"]');
+    const aedtxt = Frm.querySelector('[name="added_text"]');
+    alert(nofrows.value+" "+aedtxt.value);
+    Tbl.innerHTML=Tbl.innerHTML+" <p>"+aedtxt.value+"</p>";
 }
 
 Frm.addEventListener("submit",getFrmVale);
